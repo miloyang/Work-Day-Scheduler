31a7b27af94a70cfa907cd8067596b866dda74a5
M Yang <miloyang9@gmail.com>
2023-09-19 16:29:20 +1000

    updated html and javascript

---
+++ assets/javascript/pseudocode.js
@@ -0,0 +1,12 @@
+//When the page has been loaded, 
+//the current date should be displayed on the header. Use day.js.
+
+//Create real-time time blocks that is represented with different clours for past, present and future.
+//Use day.js for this feature.
+
+//When the user clicks on a time block, 
+//they will be able to enter text
+
+//When the save button is clicked on,
+//the event is saved in the local storage
+//Need an eventlistener for this event. 
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,57 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+var currentDayEl = $('#currentDay');
+var pastEl = $('.past');
+var presentEl = $('present');
+var futureEl = $('future');
+var saveBtnEl = $('.saveBtn');
+var timeBlockEl = $('.time-block');
+var descriptionEl = $('.description');
+
+function displayDate() {
+  var rightNow = dayjs().format('dddd, MMMM Do');
+  currentDayEl.text(rightNow);
+}
+
+displayDate();
+
+// Adding an event listener for the saveBtn 
+saveBtnEl.on('click', function () {
+if (descriptionEl.text === "") {
+  return;
+} else {
+  // Get the value of the text entered in the description box using jQuery
+  var textInput = $(this).siblings('.description').val().trim();
+  // Get the id attribute of the parent element to get the value of the time selected
+  var timeSelected = $(this).parent().attr('id');
+
+  // Details saved in local storage
+  localStorage.setItem(textInput, timeSelected);
+}
+})
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'))
+$('#hour-14 .description').val(localStorage.getItem('hour-14'))
+$('#hour-15 .description').val(localStorage.getItem('hour-15'))
+$('#hour-16 .description').val(localStorage.getItem('hour-16'))
+$('#hour-17 .description').val(localStorage.getItem('hour-17'))
+
+// function currentHour () {
+
+// }
+
+
+
+
+
+
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +70,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+// });
